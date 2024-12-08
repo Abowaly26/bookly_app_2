e1b2fdfd6d97400f6e88a63ae1ed90c7c624b814
Abowaly26 <waly20691@gmail.com>
2024-12-08 18:02:25 +0200

    add custom list vieww item to home

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tessrd/Features/home/presentation/views/widgets/custom_app_bar.dart';
+import 'package:tessrd/Features/home/presentation/views/widgets/custom_list_view_item.dart';
 import 'package:tessrd/core/utils/assets.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -10,7 +11,10 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
-      children: [CustomAppBar()],
+      children: [
+        CustomAppBar(),
+        CustomListViewItem(),
+      ],
     );
   }
 }
--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,3 +1,4 @@
 class AsssetData {
   static const logo = 'assets/images/Logo.svg';
+  static const testImage = 'assets/images/test.png';
 }
