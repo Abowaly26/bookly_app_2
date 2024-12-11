3f98b73c26479219dc544e72db36bd95cefea016
Abowaly26 <waly20691@gmail.com>
2024-12-11 12:11:06 +0200

    best seller list view item part one

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,7 +1,10 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:tessrd/Features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:tessrd/Features/home/presentation/views/widgets/custom_list_view_item.dart';
 import 'package:tessrd/Features/home/presentation/views/widgets/featured_list_view.dart';
+import 'package:tessrd/core/utils/assets.dart';
 import 'package:tessrd/core/utils/styles.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -22,7 +25,37 @@ class HomeViewBody extends StatelessWidget {
           Text(
             'Best Seller',
             style: Styles.titleMedium,
-          )
+          ),
+          BestSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 100,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.7 / 4,
+            child: Container(
+              height: MediaQuery.of(context).size.height * .25,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  color: Colors.red,
+                  image: const DecorationImage(
+                      fit: BoxFit.fill,
+                      image: AssetImage(
+                        AsssetData.testImage,
+                      ))),
+            ),
+          ),
         ],
       ),
     );
